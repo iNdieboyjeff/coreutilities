b145198803716886c5b9bd1c3d1e99bb081baa7d
Jeff Sutton <indieboy1979@gmail.com>
2015-08-25 15:18:34 +0100

    GSON ARRAY ADAPTER FACTORY FOR KEYLESS ARRAYS

---
+++ library/src/main/java/util/android/gson/ArrayAdapter.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2013-2015 Jeff Sutton.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package util.android.gson;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayAdapter<T> extends TypeAdapter<List<T>> {
+    private Class<T> adapterclass;
+
+    public ArrayAdapter(Class<T> adapterclass) {
+        this.adapterclass = adapterclass;
+    }
+
+    public List<T> read(JsonReader reader) throws IOException {
+
+        List<T> list = new ArrayList<T>();
+
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapterFactory(new ArrayAdapterFactory())
+                .create();
+
+        if (reader.peek() == JsonToken.BEGIN_OBJECT) {
+            T inning = gson.fromJson(reader, adapterclass);
+            list.add(inning);
+
+        } else if (reader.peek() == JsonToken.BEGIN_ARRAY) {
+
+            reader.beginArray();
+            while (reader.hasNext()) {
+                T inning = gson.fromJson(reader, adapterclass);
+                list.add(inning);
+            }
+            reader.endArray();
+
+        }
+
+        return list;
+    }
+
+    public void write(JsonWriter writer, List<T> value) throws IOException {
+
+    }
+
+}
---
+++ library/src/main/java/util/android/gson/ArrayAdapterFactory.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2013-2015 Jeff Sutton.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package util.android.gson;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
+public class ArrayAdapterFactory implements TypeAdapterFactory {
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Override
+    public <T> TypeAdapter<T> create(final Gson gson, final TypeToken<T> type) {
+
+        TypeAdapter<T> typeAdapter = null;
+
+        try {
+            if (type.getRawType() == List.class)
+                typeAdapter = new ArrayAdapter((Class) ((ParameterizedType) type.getType())
+                        .getActualTypeArguments()[0]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return typeAdapter;
+
+
+    }
+
+}
